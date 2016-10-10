15d6c2e68ae8b4400450e9a338d0fae5168e89eb
leejs5301 <leejs5301@naver.com>
2016-10-11 03:17:43 +0900

    fix

--- portfolio-app/myapp/public/controllers/app.main.controller.js
+++ portfolio-app/myapp/public/controllers/app.main.controller.js
@@ -13,20 +13,26 @@
         vm.goSample = goSample;
         vm.some_function = some_function;
 
+        function thisState() {
+            return appNavigator.self;
+        }
         function goHome () {
             appNavigator.goToHome();
         }
 
         function goProfile () {
             appNavigator.goTo("profile");
+            vm.some_check_value = !vm.some_check_value;
         }
 
         function goResume () {
             appNavigator.goTo("resume");
+            vm.some_check_value = !vm.some_check_value;
         }
 
         function goSample () {
             appNavigator.goTo("sample");
+            vm.some_check_value = !vm.some_check_value;
         }
 
         function some_function () {
