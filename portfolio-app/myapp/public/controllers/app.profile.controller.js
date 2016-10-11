85474b24fd867459a7f951133fd8d00edd236181
leejs5301 <leejs5301@naver.com>
2016-10-11 22:24:02 +0900

    sb1

--- portfolio-app/myapp/public/controllers/app.profile.controller.js
+++ portfolio-app/myapp/public/controllers/app.profile.controller.js
@@ -10,7 +10,9 @@
         vm.pro1_function = pro1_function;
         vm.pro2_function = pro2_function;
         vm.pro3_function = pro3_function;
-
+        vm.pro1_check_value = false;
+        vm.pro2_check_value = false;
+        vm.pro3_check_value = false;
 
         function pro1_function () {
           vm.pro1_check_value = !vm.pro1_check_value;
