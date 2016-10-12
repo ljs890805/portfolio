3ec11caf1c51a9b57d9e05198c5085877bf1afa4
leejs5301 <leejs5301@naver.com>
2016-10-12 23:26:02 +0900

    fin.fix1

--- portfolio-app/myapp/public/controllers/app.profile.controller.js
+++ portfolio-app/myapp/public/controllers/app.profile.controller.js
@@ -10,7 +10,7 @@
         vm.pro1_function = pro1_function;
         vm.pro2_function = pro2_function;
         vm.pro3_function = pro3_function;
-        vm.pro1_check_value = false;
+        vm.pro1_check_value = true;
         vm.pro2_check_value = false;
         vm.pro3_check_value = false;
 
